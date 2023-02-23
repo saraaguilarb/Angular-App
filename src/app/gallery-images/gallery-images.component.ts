dcd49e67220f65a4dace39a53c088508bb19cb29
saraaguilarb <122842897+saraaguilarb@users.noreply.github.com>
2023-02-22 22:35:06 -0600

    Third commit

---
+++ src/app/gallery-images/gallery-images.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-images',
+  templateUrl: './gallery-images.component.html',
+  styleUrls: ['./gallery-images.component.css']
+})
+export class GalleryImagesComponent {
+
+}
